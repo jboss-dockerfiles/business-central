f2a055dbd17e609be18d2186f8b09d90c4a95a19
Michael Biarnes Kiefer <mbiarnes@redhat.com>
2019-10-28 13:21:22 +0100

    removed obsolete KIE_demo reference

--- base/etc/start_business-central-wb.sh
+++ base/etc/start_business-central-wb.sh
@@ -2,5 +2,5 @@
 
 # Start Wildfly with the given arguments.
 echo "Running business-central workbench on JBoss Wildfly..."
-exec ./standalone.sh -b $JBOSS_BIND_ADDRESS -c $KIE_SERVER_PROFILE.xml -Dorg.kie.demo=$KIE_DEMO -Dorg.kie.example=$KIE_DEMO
+exec ./standalone.sh -b $JBOSS_BIND_ADDRESS -c $KIE_SERVER_PROFILE.xml
 exit $?
