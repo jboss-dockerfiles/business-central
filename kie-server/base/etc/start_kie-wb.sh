1e4bc273fdc4688d84f73311e1cc54f3f19e1843
Michael Biarnes Kiefer <mbiarnes@redhat.com>
2019-11-28 13:15:38 +0100

    upgraded to 7.30.0.Final

--- kie-server/base/etc/start_kie-wb.sh
+++ kie-server/base/etc/start_kie-wb.sh
@@ -2,5 +2,5 @@
 
 # Start Wildfly with the given arguments.
 echo "Running jBPM Workbench on JBoss Wildfly..."
-exec ./standalone.sh -b $JBOSS_BIND_ADDRESS -c standalone-full.xml
+exec ./standalone.sh -b $JBOSS_BIND_ADDRESS -c standalone-full.xml -Djava.net.preferIPv4Stack=true -Djava.net.preferIPv4Addresses=true
 exit $?
