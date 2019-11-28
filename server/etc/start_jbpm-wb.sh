f3e6a023256e0be1b79e5e92c95757d5f56e9c28
Michael Biarnes Kiefer <mbiarnes@redhat.com>
2019-11-28 13:08:10 +0100

    tag for 7.30.0.Final

--- server/etc/start_jbpm-wb.sh
+++ server/etc/start_jbpm-wb.sh
@@ -4,5 +4,5 @@
 echo "Update database connection setup"
 ./update_db_config.sh
 echo "Running jBPM Server Full on JBoss Wildfly..."
-exec ./standalone.sh -b $JBOSS_BIND_ADDRESS $EXTRA_OPTS -Dorg.kie.server.location=$KIE_SERVER_LOCATION -Dorg.kie.server.id=$KIE_SERVER_ID
+exec ./standalone.sh -b $JBOSS_BIND_ADDRESS $EXTRA_OPTS -Dorg.kie.server.location=$KIE_SERVER_LOCATION -Dorg.kie.server.id=$KIE_SERVER_ID -Djava.net.preferIPv4Stack=true -Djava.net.preferIPv4Addresses=true
 exit $?
